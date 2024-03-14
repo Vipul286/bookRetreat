fb2f76243606192432515cef2b73b172dd051b77
moremk <159239635+moremk@users.noreply.github.com>
2024-03-14 16:35:23 +0530

    Update pdata.php

--- pdata.php
+++ pdata.php
@@ -9,7 +9,7 @@ if(isset($_POST['nf'])&&isset($_POST['nl'])&&isset($_POST['sl'])&&isset($_POST['
     $cvv=$_POST['cvv'];
     $ce=$_POST['ce'];
     $am=$_POST['am'];
-    $con=new mysqli('localhost','root','','flat');
+   $con=new mysqli('myazdemosvr.mysql.database.azure.com','myadmin@myazdemosvr','Server@1','flat');
     $q="insert into pay1(FName,LName,Bank,Creditcardnumber,Securitycode,Cardexpiration,Amount)values('$nf','$nl','$sl','$cnn','$cvv','$ce','$am')";
     $r=mysqli_query($con,$q);
     if($r)
