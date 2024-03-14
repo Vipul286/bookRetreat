21d59f89614838ba1fb1dfa655e09d370ea171b0
moremk <159239635+moremk@users.noreply.github.com>
2024-03-14 16:34:49 +0530

    Update cont.php

--- cont.php
+++ cont.php
@@ -5,7 +5,7 @@ $nm=$_POST['nm'];
 $em=$_POST['em'];
 $mb=$_POST['mb'];
 $ad=$_POST['ad'];
-$con=new mysqli('localhost','root','','flat');
+$con=new mysqli('myazdemosvr.mysql.database.azure.com','myadmin@myazdemosvr','Server@1','flat');
 $q="insert into contact(Name,Email,Contact,Adress)values('$nm','$em','$mb','$ad')";
 $r=mysqli_query($con,$q);
 }
